dbd1db1a660ef1e38a5a995b86d6ea07efa6b18b
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2022-11-28 19:52:45 +0700

    add Transaction class

---
+++ Transaction.py
@@ -0,0 +1,30 @@
+from tabulate import tabulate
+
+class Transaction():
+    def __init__(self):
+        self.items = dict()
+    
+    def add_item(self, name, qty, price):
+        pass
+
+    def update_item_name(self, name, name_update):
+        pass
+
+    def update_item_qty(self, name, qty_update):
+        pass
+
+    def update_item_price(self, name, price_update):
+        pass
+
+    def delete_item(self, name):
+        pass
+
+    def reset_transaction(self):
+        pass
+
+    def check_order(self):
+        pass
+
+    def total_price(self):
+        pass
+    
